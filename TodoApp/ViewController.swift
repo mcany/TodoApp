383f84026eafb07173f595df060b9a53fd20faa3
urklc <ugurkilic@live.com>
2019-09-05 19:08:56 +0300

    Initial commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Ugur Kilic on 5/09/2019.
+//  Copyright © 2019 urklc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
